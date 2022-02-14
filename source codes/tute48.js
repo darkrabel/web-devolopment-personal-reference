9f40172ec3e1ee57103ab2067aac44f8df317f3d
faheem <faheemeducation2020@gmail.com>
2022-02-14 09:33:44 +0600

    intial commit

---
+++ source codes/tute48.js
@@ -0,0 +1,21 @@
+var a =78; //this is the way of writing variable in js
+        
+        console.log(a);
+        c=45.566
+        console.log(c)
+
+        //_________________________
+
+        //operators in javascirpt
+
+
+
+        var num1 = 56;
+        var num2 = 54;
+        console.log("The vale of num1 and num2 is "+(num1+num2));
+
+
+        //increment operator
+        console.log("this will print the value than increment is it will not show the increament value "+(num1++));
+        console.log("this will increament the value than print. it will  show the increament value "+(num1++));
+        //we can also use --
---
+++ source codes/tute49.js
@@ -0,0 +1,2 @@
+> git -c user.useConfigOnly=true commit --quiet --allow-empty-message --file -
+fatal: cannot lock ref 'HEAD': unable to resolve reference 'refs/heads/main': reference broken
